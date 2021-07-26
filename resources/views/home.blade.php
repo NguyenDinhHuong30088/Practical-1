b1eebdc301bbcb1d5df96370acdef23800c3f2b4
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2021-07-26 11:03:15 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>My Project</title>
+    <title>Survey</title>
 </head>
 <body>
 <div class="d-flex justify-content-center">
-    <h1>Survey Form</h1>
+    <h1>Khảo Sát ý kiến sinh viên</h1>
 </div>
 <div class="container">
     <div class=" justify-content-center">
